8e9e7aa474374d889f8f6f1de382753d9f91300e
Dawid Paszko <dawidpl8@o2.pl>
2022-03-26 17:56:08 +0100

    dodanie zwalniania pamięci, małe poprawki

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ typedef struct CString
 CString* cstring_new(const char* str)
 {
 	size_t size = strlen(str);
-	CString* q = malloc(sizeof(CString));
+	CString* q = (CString*)malloc(sizeof(CString));
 	char* buffer = malloc(size+1);
 	strcpy(buffer, str);
 	q->text = buffer;
@@ -42,17 +42,27 @@ void cstring_set(CString* str, int pos, char a)
 		str->text[pos] = a;
 }
 
+void cstring_free(CString* str)
+{
+	if(str!=NULL)
+	{
+		free(str->text);
+		free(str);
+	}
+}
+
 int main()
 {
 	CString* a;
 	assert(a = cstring_new("kreda"));
-	char* t1 = cstring_text(a);
+	const char* t1 = cstring_text(a);
 	printf("%s\n", t1);
 	printf("%zu\n", cstring_length(a));
 	printf("%c\n", cstring_get(a, 0));
 	cstring_set(a, 3, 't');
-	char* t2 = cstring_text(a);
+	const char* t2 = cstring_text(a);
 	printf("%s\n", t2);
+	cstring_free(a);
 	
 	return 0;
 }
